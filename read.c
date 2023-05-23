a6f8b8e04abafaa73f9191b596da91308f95ee42
GraceBuri <grcbuuri@gmail.com>
2023-05-23 09:43:16 -0700

    Add first shell

---
+++ read.c
@@ -0,0 +1,42 @@
+#include "shell.h"
+/**
+ * read_command - a function that reads and tokenizes user input
+ * @cmd : an array storing user commands
+ * @par: an array of pointers to user parameters
+ * Return: void
+ */
+void read_command(char cmd[], char *par[])
+{
+	char *pch, *line = NULL;
+	size_t len = 0;
+	ssize_t nread;
+	int i = 0, j;
+
+	signal(SIGINT, SIG_IGN);
+	nread = getline(&line, &len, stdin);
+
+	if (nread != -1)
+	{
+		line[nread - 1] = '\0';
+		if (_strlen(line) > 0)
+		{
+			pch = strtok(line, " ");
+			while (pch != NULL)
+			{
+				par[i++] = _strdup(pch);
+				pch = strtok(NULL, " ");
+			}
+			_strcpy(cmd, par[0]);
+			par[i] = NULL;
+		}
+
+	}
+	else
+	{
+		write(1, "Exiting success\n", 16);
+		exit(EXIT_SUCCESS);
+	}
+	for (j = 0; j < i; j++)
+		free(par[j]);
+	free(line);
+}
---
+++ shell.h
@@ -0,0 +1,30 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+extern char **environ;
+
+void type_prompt(void);
+void read_command(char cmd[], char *par[]);
+void execute_command(char *command, char **parameters);
+size_t my_getline(char *line, size_t size, FILE *stream);
+
+int _strlen(char *s);
+char *_strcat(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+char *_strcpy(char *dest, char *src);
+char *_strdup(const char *str);
+
+int _atoi(char *s);
+
+#endif
+
